391c4a1629f1c675eb6b648d7a7d4dcd99b5fb8d
KOYAMA Tetsuji <koyama@hoge.org>
2017-02-15 18:24:47 +0900

    Support feature to recieve push notifications.

--- app/src/main/java/org/koyhoge/ncmboard/MainActivity.java
+++ app/src/main/java/org/koyhoge/ncmboard/MainActivity.java
@@ -17,11 +17,14 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.nifty.cloud.mb.core.DoneCallback;
+import com.nifty.cloud.mb.core.FindCallback;
 import com.nifty.cloud.mb.core.LoginCallback;
 import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBException;
+import com.nifty.cloud.mb.core.NCMBInstallation;
 import com.nifty.cloud.mb.core.NCMBObject;
 import com.nifty.cloud.mb.core.NCMBObjectService;
+import com.nifty.cloud.mb.core.NCMBQuery;
 import com.nifty.cloud.mb.core.NCMBUser;
 
 import java.util.ArrayList;
@@ -50,6 +53,33 @@ public class MainActivity extends AppCompatActivity {
         String app_key = "xxxxxxxx";
         String client_key = "xxxxxxxx";
         NCMB.initialize(this, app_key, client_key);
+
+        final NCMBInstallation installation = NCMBInstallation.getCurrentInstallation();
+
+        //GCMからRegistrationIdを取得しinstallationに設定する
+        String fcm_sender_id ="xxxxxxxx";
+        installation.getRegistrationIdInBackground(fcm_sender_id, new DoneCallback() {
+            @Override
+            public void done(NCMBException e) {
+                if (e == null) {
+                    installation.saveInBackground(new DoneCallback() {
+                        @Override
+                        public void done(NCMBException e) {
+                            if (e == null) {
+                                //保存成功
+                            } else if (NCMBException.DUPLICATE_VALUE.equals(e.getCode())) {
+                                //保存失敗 : registrationID重複
+                                updateInstallation(installation);
+                            } else {
+                                //保存失敗 : その他
+                            }
+                        }
+                    });
+                } else {
+                    //ID取得失敗
+                }
+            }
+        });
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -113,6 +143,31 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+    /**
+     * Update installation data with own device.
+     * @param installation NCMBInstallation instance
+     */
+    public static void updateInstallation(final NCMBInstallation installation) {
+        //installationクラスを検索するクエリの作成
+        NCMBQuery<NCMBInstallation> query = NCMBInstallation.getQuery();
+
+        //同じRegistration IDをdeviceTokenフィールドに持つ端末情報を検索する
+        query.whereEqualTo("deviceToken", installation.getDeviceToken());
+
+        //データストアの検索を実行
+        query.findInBackground(new FindCallback<NCMBInstallation>() {
+            @Override
+            public void done(List<NCMBInstallation> results, NCMBException e) {
+
+                //検索された端末情報のobjectIdを設定
+                installation.setObjectId(results.get(0).getObjectId());
+
+                //端末情報を更新する
+                installation.saveInBackground();
+            }
+        });
     }
 
     /**
